07d8639dd30edce12ed0410c2d43c94dc91babad
sσннкα <40715791+Sohhka@users.noreply.github.com>
2020-09-24 17:49:24 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+games { 'gta5' };
+description 'Loading screen pour Las Venturas | by .Sohhka#8888'
+
+files {
+    'index.html',
+    'style.css',
+    'animation.js',
+    'ville.gif',
+    'menu.js',
+    'about.html',
+    'contact.html',
+    'lv_logo2.png'
+}
+
+loadscreen 'index.html'
